5b391056c4357bf72df1f57b75ba757c5d60bd08
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2022-11-28 00:28:43 +0530

    Code to create a sensor to identify API is available

---
+++ code_docs/create_sensor.py
@@ -0,0 +1,31 @@
+#import the DAG object
+from airflow import DAG        
+from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.sensors.http import HttpSensor
+
+#define unique DAG Id, start date, schedule interval, catchup
+with DAG('user_processing', start_date = datetime(2022, 1, 1), 
+    schedule_interval = "@daily", catchup = False) as dag:
+  
+    #create table, unique task id, sql request
+    create_table = PostgresOperator(
+        task_id = 'create_table',
+        postgres_conn_id = 'postgres',
+        sql = '''
+            CREATE table IF NOT EXISTS users {
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            };
+        '''
+    )
+
+    #task to identify API is available or not
+    is_api_available = HttpSensor(
+        task_id = 'is_api_available', 
+        http_conn_id = 'user_api,
+        endpoint = 'api/')
