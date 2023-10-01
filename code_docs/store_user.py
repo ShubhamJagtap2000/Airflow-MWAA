f81d42708483a67b0a65b2f7e1c1778f8ae0b47a
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2023-10-01 23:05:46 +0530

    Update store_user.py

--- code_docs/store_user.py
+++ code_docs/store_user.py
@@ -1,15 +1,12 @@
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-...
 def _store_user():
     hook = PostgresHook(postgres_conn_id='postgres')
     hook.copy_expert(
         sql="COPY users FROM stdin WITH DELIMITER as ','",
         filename='/tmp/processed_user.csv'
     )
-
-...
-
+    
     store_user = PythonOperator(
         task_id='store_user',
         python_callable=_store_user
